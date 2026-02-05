c107fedfe464735426d85611e95c095fb4220ec3
Nikhil Raj Singh <nikhilraj1169@gmail.com>
2026-02-04 19:26:05 -0800

    Added manual timeout error for testing

--- go-http/service-a/main.go
+++ go-http/service-a/main.go
@@ -18,8 +18,8 @@ func echo(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
 	msg := r.URL.Query().Get("msg")
 
-	// Simulate processing time (optional, but good for testing timeouts later)
-	// time.Sleep(50 * time.Millisecond)
+	// To Simulate processing time (optional, but good for testing timeouts uncomment the below line)
+	//time.Sleep(2000 * time.Millisecond)
 
 	w.Header().Set("Content-Type", "application/json")
 	_ = json.NewEncoder(w).Encode(map[string]string{"echo": msg})
